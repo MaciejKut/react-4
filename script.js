ac6f20eb2d4e7dd337e030e74e9494330ac312e9
Maciej Kutzmann <maciej.kutzmann@gmail.com>
2019-03-26 15:27:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,32 @@
 var Counter = React.createClass({
-    getDefaultProps() {
-        console.log('To load some data in case if no data we provided')
-    },
-    getInitialState: function () {
+    getInitialState: function() {
         return {
             counter: 0
         };
     },
-    increment: function () {
+    increment: function() {
         this.setState({
-            counter: this.state.counter + 1
+            counter: this.state.counter +1
         });
     },
-    decrement: function () {
+    decrement: function(){
         this.setState({
-            counter: this.state.counter - 1
-
-        });
+            counter: this.state.counter-1
+        })
     },
 
-    componentWillMount() {
-        console.log('deprecated')
-    },
-    componentDidMount() {
-        console.log(' ComponentDidMount W chwili wykonywania tej metody, nasz komponent widnieje już na stronie (jest zamontowany w drzewie DOM). Możemy wykonywać na nim różne operacje manipulacji, używać jQuery albo też pobrać dane. Dlaczego pobieramy dane dopiero w tym miejscu? Dlatego, że chcemy jak najszybciej pokazać nasze komponenty użytkownikowi. Nie możemy sobie pozwolić na wstrzymanie działania aplikacji tylko dlatego, że czekamy na odpowiedź z serwera.')
-        console.log('Czy tu możemy użyć tych kolejnych metod?');
-    },
-    render: function () {
-        return React.createElement('div', { className: 'counters' },
+    render: function() {
+        return React.createElement('div', {},
+            React.createElement('span', {}, 'Licznik ' + this.state.counter),
+            React.createElement('span', {className: 'increment', onClick: this.increment}, 'Increment'),
+            React.createElement('span', {className: 'decrement', onClick: this.decrement}, 'Decrement')
 
-            React.createElement('div', { onClick: this.increment, className: 'counter' }, 'Increment'),
-            React.createElement('div', {}, 'Counter value: ' + this.state.counter),
-            React.createElement('div', { onClick: this.decrement, className: 'counter1' }, 'Decrement')
-        )
-
+        );
     }
-});
-
-var Counter2 = React.createClass({
-    getInitialState: function () {
-        return {
-            counter: 0
-        };
-    },
-    increment: function () {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    },
-    decrement: function () {
-        this.setState({
-            counter: this.state.counter - 1
-
-        });
-    },
-    render: function () {
-        return React.createElement('div', { className: 'counters' },
-
-            React.createElement('div', { onClick: this.increment, className: 'counter' }, 'Increment'),
-            React.createElement('div', {}, 'Counter value: ' + this.state.counter),
-            React.createElement('div', { onClick: this.decrement, className: 'counter1' }, 'Decrement')
-        )
-
-    }
-});
-
-
-var App = React.createClass({
-    render: function () {
-        return React.createElement('div', { className: 'app' },
-
-            React.createElement(Counter),
-            React.createElement(Counter2),
-            React.createElement(Counter),
-            React.createElement(Counter2)
-        )
-    }
-})
-
-
-var element = React.createElement(App);
-ReactDOM.render(element, document.getElementById('app'));
+  });
+  
+  var element = React.createElement('div',{},
+        React.createElement(Counter),
+        React.createElement(Counter)
+  );
+  ReactDOM.render(element, document.getElementById('app'));
